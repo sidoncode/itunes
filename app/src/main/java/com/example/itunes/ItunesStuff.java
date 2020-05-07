fcf7679b6d3c4bf6b53929bb2662191cd2d25622
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-05-07 21:00:49 +0530

    getter and setter new class json

---
+++ app/src/main/java/com/example/itunes/ItunesStuff.java
@@ -0,0 +1,60 @@
+package com.example.itunes;
+
+public class ItunesStuff {
+
+
+    private String type;
+    private String kind;
+    private String artisitName;
+    private String collectionName;
+    private String trackName;
+    private String artisitViewURL;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getArtisitName() {
+        return artisitName;
+    }
+
+    public void setArtisitName(String artisitName) {
+        this.artisitName = artisitName;
+    }
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtisitViewURL() {
+        return artisitViewURL;
+    }
+
+    public void setArtisitViewURL(String artisitViewURL) {
+        this.artisitViewURL = artisitViewURL;
+    }
+}
